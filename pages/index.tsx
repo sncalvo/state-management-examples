2b65af3ea00716af6697e852b52181922e2e279e
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2022-07-30 16:28:00 -0300

    Add quick react example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,21 @@
 import type { NextPage } from 'next';
+import { useState } from 'react';
+import { Button } from '../src/context/components/atoms';
 
-const Home: NextPage = () => <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+const Home: NextPage = () => {
+  const [text, setText] = useState('');
+
+  const setRandomNumber = () => {
+    setText(`${Math.random()}`);
+  };
+
+  return (
+    <div className="flex flex-col gap-2 p-4">
+      <h1 className="text-3xl underline">Home</h1>
+      <Button onClick={setRandomNumber}>Set Random Text</Button>
+      <h2>{text}</h2>
+    </div>
+  );
+};
 
 export default Home;
